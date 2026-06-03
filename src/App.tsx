308e94466907f4d7d0fafb6b91770ac204238b6e
melvin.php.ph <melvin.php.ph@tripleonetech.net>
2026-06-03 18:54:18 +0800

    start r3ftut

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div className="">
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        
+      </section>
+    </>
+  )
+}
+
+export default App
